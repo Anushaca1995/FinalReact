f83335b00f6f8c1aaba7c3e5119cca63ade66129
Anusha Antony <anushaca1995@gmail.com>
2022-12-06 16:08:32 +0000

    Added api for sign in functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,19 @@ import SignUp from './container/SignUp/SignUp';
 import SignIn from './container/SignIn/SignIn';
 import BookIn from './container/BookIn/BookIn';
 import User from './components/User/User';
-import UserList from './container/UserList/UserList';
 import ViewUser from './container/ViewUser/ViewUser';
+import { useState } from 'react';
 function App() {
+  const [user, setUser] = useState(false);
   return (
     <div className="App">
      <Router>
       <Routes>
-        <Route path="*" element={<Home/>} />
-        <Route path="/signup" element={<SignUp/>} />
-        <Route path="/login" element={<SignIn/>} />
-        <Route path="/bookin" element={<BookIn/>} />
-        <Route path="/userlist" element={<ViewUser/>} />
+        <Route path="*" element={<Home user={user}/>} />
+        <Route path="/signup" element={<SignUp user={user}/>} />
+        <Route path="/login" element={<SignIn saveUser={setUser}/>} />
+        <Route path="/bookin" element={<BookIn user={user}/>} />
+        <Route path="/userlist" element={<ViewUser user={user}/>} />
         <Route path="/user" element={<User/>} />
       </Routes>
      </Router>
